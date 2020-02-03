331102d34db869dde66a242dc5aea597b49f7f77
Anna <asylvester@bitpay.com>
2020-02-02 21:39:47 -0500

    change root to index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <PortfolioHero/>
+    <!--    <PortfolioDivide/>-->
+  </div>
+</template>
+
+<script>
+    import NavBar from '../components/portfolio--nav-bar.vue';
+    import PortfolioHero from '../components/portfolio--hero.vue'
+    // import PortfolioDivide from './components/portfolio--divide.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            NavBar,
+            PortfolioHero,
+            // PortfolioDivide
+        }
+    }
+</script>
+
+<style>
+  @import "../styles/main.scss";
+</style>
